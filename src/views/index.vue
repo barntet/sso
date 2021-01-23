4ad2f244f64ae13e3d2cc444bbe62029ddf71af9
罗旭宏 <barntet@163.com>
2021-01-23 16:27:18 +0800

    init

---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a-button type="primary">
+      Primary
+    </a-button>
+  </div>
+</template>
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component()
+
+export default class Index extends Vue {
+
+}
+</script>
+<style lang="module"></style>
